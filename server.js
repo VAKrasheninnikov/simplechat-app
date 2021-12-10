86de529e6fa75a3af884180023ee0a05c7848ac3
VAKrasheninnikov <vlad-krasheninni@mail.ru>
2021-12-10 19:07:34 +0300

    10.12

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.post('/rooms', (req, res)=>{
 })
 
 io.on('connection', socket => {
+    socket.on('ROOM:JOIN'), ({roomId, userName}) => {
+        socket.join(roomId)
+        rooms.get(roomId).get('users').socket(socket.id, userName)
+    }
     console.log('user connected', socket.id)
 })
 
